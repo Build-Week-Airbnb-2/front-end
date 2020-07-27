077f17b7720e15dd925408873d1cdcfb5eb09a81
Isaiah <greatpup1940@gmail.com>
2020-07-27 14:36:22 -0400

    Created the markup for register component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Register from './components/register/register'
 
-import 'reset-css'
+
+import 'reset-css';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Airbnb Pricing Calculator</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Route exact path="/" render={() => <h1>Home Component</h1>}></Route>
+      <Route path="/register" render={() => <Register></Register>}></Route>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/register/register.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './register.styles.css';
+import { Link, Redirect } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { TextField, Select, MenuItem, InputLabel, Checkbox } from '@material-ui/core';
+import RootRef from '@material-ui/core/RootRef';
+
+import * as Yup from 'yup';
+
+
+
+
+
+const Register = () => {
+   
+
+    
+    return(
+        <div className="form-container">
+            <h1>Register</h1>
+            <form className="form" >
+              
+                {/* <input type="text" name="" className='name' id="" placeholder="Isaiah"/> */}
+
+                <TextField
+                    className="email"
+                    type="email"
+                    name="email"
+                    id="name"
+                    label="email"
+                />
+                <TextField
+                    className="password"
+                    type="password"
+                    name="password"
+                    id="name"
+                    label="password"
+                />
+
+                {/* <Button type="submit" value="Submit" onSubmit={handleSubmit} color='primary'>Add Teammate</Button> */}
+               
+               
+
+                <Button type="submit" variant="contained" color="secondary">
+                    REGISTER
+				</Button>
+            </form>
+            <Link className="bottom-link" to="/team">
+                <Button>Login</Button>
+            </Link>
+        </div>
+    )
+}
+
+export default Register
