704c2c9cc2d47589290b93e031b3e8c7f38963b2
Isaiah <greatpup1940@gmail.com>
2020-07-28 14:27:05 -0400

    Nav component is finished

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -6,6 +6,8 @@ import { TextField, Select, MenuItem, InputLabel, Checkbox } from '@material-ui/
 import RootRef from '@material-ui/core/RootRef';
 import axios from 'axios';
 import * as Yup from 'yup';
+import Navbar from '../navbar/navbar'
+
 
 const Register = () => {
 	let [ formValues, setFormValues ] = useState({
@@ -66,8 +68,9 @@ const Register = () => {
 
 	return (
 		<div className="form-container">
+			<Navbar></Navbar>
+
 			<form className="form" onSubmit={handleSubmit}>
-				<h1>Register</h1>
 				<div className="field-container">
 					<TextField
 						className="email"
@@ -95,9 +98,7 @@ const Register = () => {
 					REGISTER
 				</Button>
 			</form>
-			<Link className="bottom-link" to="/login">
-				<Button>Login</Button>
-			</Link>
+			
 		</div>
 	);
 };
